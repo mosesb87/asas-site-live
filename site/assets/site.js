4d4d9c4c1b9e1439e4aa90c36c05391d7c358097
mosesb87 <musa.f.batarseh@gmail.com>
2026-09-24 13:45:31 -0400

    Desktop screenshots for page cards

--- site/assets/site.js
+++ site/assets/site.js
@@ -304,7 +304,7 @@
   function pageHTML(p) {
     return `<article class="card card--page" data-slug="${p.slug}" data-type="${p.type}" data-q="${(p.name + ' ' + p.gloss + ' ' + p.type + ' ' + p.ar_name).toLowerCase()}">
       <a class="card__thumb" href="https://asas.build/studio/?stack=${p.recipe.join(',')}${AR ? '&g=ar' : ''}" data-cursor="${T.open}" aria-label="${T.pageAria(AR ? p.ar_name : p.name, typeName(p.type))}">
-        <img src="${p.img}" alt="${T.pageAlt(typeName(p.type), AR ? p.ar_name : p.name, T.blocks(p.blocks))}" loading="lazy" width="780" height="1170">
+        <picture>${/\/img\/pages\//.test(p.img) ? `<source media="(min-width: 701px)" srcset="${p.img.replace('.webp', '-d.webp')}">` : ''}<img src="${p.img}" alt="${T.pageAlt(typeName(p.type), AR ? p.ar_name : p.name, T.blocks(p.blocks))}" loading="lazy" width="780" height="1170"></picture>
       </a>
       <div class="card__meta">
         ${AR ? `<div class="card__name">${p.ar_name}<span class="lat">${p.name}</span><small>صفحة ${typeName(p.type)}</small></div>`
